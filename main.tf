bb6eda9b0a17ca0cdddeb3a28171b442075a2a80
kushal singh <kushalvir@hotmail.com>
2018-10-25 11:27:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,72 +1,11 @@
-# Grab the ARN of the current logged in user
-data "aws_caller_identity" "current" {}
-
-# create a role which allows the current user to assume it
-resource "aws_iam_role" "terraform_11270" {
-  name = "terraform_11270"
-  path = "/test/"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    },
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "${data.aws_caller_identity.current.arn}"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "terraform_11270" {
-  name = "terraform_11270"
-  role = "${aws_iam_role.terraform_11270.id}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:*",
-        "ec2:*"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-# configure this provider alias to only use the IAM Role created above
 provider "aws" {
-  alias = "iamrole"
-
+  version = "~> 1.4"
+  region  = "us-east-1"
+  profile = "default"
   assume_role {
-    role_arn = "${aws_iam_role.terraform_11270.arn}"
+    role_arn = "arn:aws:iam::337089113773:role/jenkins-slave-prof-role"
   }
 }
-
 resource "aws_security_group" "primary" {
-  name = "primary"
-}
-
-# Create a security group with the above IAM Role assumed
-resource "aws_security_group" "secondary" {
-  provider = "aws.iamrole"
-  name     = "secondary"
+  name = "primary-test-sg"
 }
