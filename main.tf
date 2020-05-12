1c2ed3cd6eab9a35a8f7936e767069404a26e981
kushal singh <kushalvir@hotmail.com>
2020-05-12 17:57:52 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region  = "us-east-1"
   #profile = "default"
   assume_role {
-    role_arn = "arn:aws:iam::304951274516:role/dns-deployment-role"
+    role_arn = "arn:aws:iam::*******:role/dns-deployment-role"
   }
 }
 resource "aws_security_group" "primary" {
